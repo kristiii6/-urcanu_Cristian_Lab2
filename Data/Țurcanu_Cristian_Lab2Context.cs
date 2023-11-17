571e33a8e334cd143beca984a5a88b494fac4d92
Cristian Țurcanu <cristian.turcanu27@gmail.com>
2023-11-17 02:16:48 +0100

    Add project files.

---
+++ Data/Țurcanu_Cristian_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Țurcanu_Cristian_Lab2.Models;
+
+namespace Țurcanu_Cristian_Lab2.Data
+{
+    public class Țurcanu_Cristian_Lab2Context : DbContext
+    {
+        public Țurcanu_Cristian_Lab2Context (DbContextOptions<Țurcanu_Cristian_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Țurcanu_Cristian_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Țurcanu_Cristian_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
